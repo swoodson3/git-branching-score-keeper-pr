f475e1ce12a01e3f7643268d0de604518a2b10f6
Seth Woodson <sethwoodson3@gmail.com>
2023-03-29 20:44:04 -0500

    added-color

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 5000;
+const PORT = 5012;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
